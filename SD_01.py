166489c9a8b2c81d24d9c48433f278fff4041198
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-01-08 23:11:28 +0530

    functions to convert added

--- SD_01.py
+++ SD_01.py
@@ -1,6 +1,28 @@
 from tkinter import *
 
+def celsius_to_kelvin(celsius):
+    kelvin = celsius + 273.15
+    return kelvin
 
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def fahrenheit_to_kelvin(fahrenheit):
+    kelvin = (fahrenheit + 459.67) * 5/9
+    return kelvin
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def kelvin_to_celsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+def kelvin_to_fahrenheit(kelvin):
+    fahrenheit = (kelvin * 9/5) - 459.67
+    return fahrenheit
 
 
 def convert():
